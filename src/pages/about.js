73ef7d74e41d0d33cdac731080a72788ffd2b43e
Alex Nielsen <alex.nielsen@gmail.com>
2023-07-29 23:38:10 +0000

    finished 2

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,19 +1,23 @@
-// Step 1: Import React
-import * as React from 'react';
+import * as React from 'react'
+import Layout from '../components/layout'
 
-// Step 2: Define your component
 const AboutPage = () => {
   return (
-    <main>
-      <h1>About Me</h1>
-      <p>
-        Hi there! I'm the proud creator of this site, which I built with Gatsby.
-      </p>
-    </main>
-  );
-};
+    <>
+      <Layout pageTitle='About Me'>
+        <p>
+          Hi there! I'm the proud creator of this site, which I built with
+          Gatsby.
+        </p>
+        <p>in layout on about.js</p>
+      </Layout>
+      <div>
+        <p>outside of layout on about</p>
+      </div>
+    </>
+  )
+}
 
-export const Head = () => <title>About Me</title>;
+export const Head = () => <title>About Me</title>
 
-// Step 3: Export your component
-export default AboutPage;
+export default AboutPage
