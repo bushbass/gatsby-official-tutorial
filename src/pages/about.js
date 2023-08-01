5756994aae7e0b591beeee874b3d79dc1fca0273
Alex Nielsen <alex@bigal.org>
2023-07-31 20:22:54 -0400

    finished part 4

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,11 @@
-import * as React from 'react'
-import Layout from '../components/layout'
+import * as React from 'react';
+import Layout from '../components/layout';
+import Seo from '../components/seo';
 
 const AboutPage = () => {
   return (
     <>
-      <Layout pageTitle='About Me'>
+      <Layout pageTitle="About Me">
         <p>
           Hi there! I'm the proud creator of this site, which I built with
           Gatsby.
@@ -15,9 +16,9 @@ const AboutPage = () => {
         <p>outside of layout on about</p>
       </div>
     </>
-  )
-}
+  );
+};
 
-export const Head = () => <title>About Me</title>
+export const Head = () => <Seo title="About Me" />;
 
-export default AboutPage
+export default AboutPage;
